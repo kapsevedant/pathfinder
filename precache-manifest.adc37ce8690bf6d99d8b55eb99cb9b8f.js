f6193ee6e42db28e00dcce6fd07d181727248212
unknown <vedantkapse2020@gmail.com>
2023-09-10 22:38:21 +0530

    Updates

---
+++ precache-manifest.adc37ce8690bf6d99d8b55eb99cb9b8f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f653b74899e0968b4e9f214549e911",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "48768cb3c404402cd79b",
+    "url": "/pathfinder/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "691339bc645e3772822c",
+    "url": "/pathfinder/static/css/main.ed19af3e.chunk.css"
+  },
+  {
+    "revision": "48768cb3c404402cd79b",
+    "url": "/pathfinder/static/js/2.3bab983f.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/pathfinder/static/js/2.3bab983f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691339bc645e3772822c",
+    "url": "/pathfinder/static/js/main.f7e57d09.chunk.js"
+  },
+  {
+    "revision": "95e86ac06044e0df6349",
+    "url": "/pathfinder/static/js/runtime-main.137dc044.js"
+  },
+  {
+    "revision": "4cff88f92453b46e9a8ddfd929e27aa4",
+    "url": "/pathfinder/static/media/favicon.4cff88f9.png"
+  },
+  {
+    "revision": "be3dbdd87160c4172c2873c218644e77",
+    "url": "/pathfinder/static/media/s.be3dbdd8.jpg"
+  }
+]);
